67f4e6520feb7aea3c4699e7cdf59010d7d0aa7f
jiajunfanthu <345684710@qq.com>
2022-03-04 18:35:58 +0800

    js finish

--- javascript /counter.js
+++ javascript /counter.js
@@ -1,16 +1,26 @@
-let counter = 0;
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
+// // 
+// localStorage.setItem('counter', 0);
 
 function count() {
-    counter = counter + 1;
+    let counter = localStorage.getItem('counter');
+    counter++;
     document.querySelector('h1').innerHTML = counter;
-    if (counter % 10 === 0) {
-        alert(`count is now ${counter}`);
-    }
+
+    localStorage.setItem('counter', counter);
+
+
+    // if (counter % 10 === 0) {
+    //     alert(`count is now ${counter}`);
+    // }
 
 }
 document.addEventListener('DOMContentLoaded',
     function() {
+        document.querySelector('h1').innerHTML = localStorage.getItem('counter');
         document.querySelector('button').onclick = count;
-
+        // setInterval(count, 1);
     }
 );
